0a61ea1a45b7f1f756347812b3e3830dcb0708c4
yashwanthkumards1993 <anushamanjula20@gmail.com>
2026-05-04 17:11:17 +0400

    fix: update git config email for vercel

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,35 +1,70 @@
-import { assets, infoList, toolsData } from '@/assets/assets'
-import Image from 'next/image'
-import React from 'react'
-import { motion } from "motion/react"
+import { assets, infoList, toolsData } from "@/assets/assets";
+import Image from "next/image";
+import React from "react";
+import { motion } from "motion/react";
 
 const About = ({ isDarkMode }) => {
-    return (
-        <motion.div id="about" className='w-full px-[12%] py-5 scroll-mt-20'
-            initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 1 }}>
+  return (
+    <motion.div
+      id="about"
+      className="w-full px-[12%] py-5 scroll-mt-20"
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
+      <motion.h4
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.2 }}
+        className="text-center mb-2 text-lg font-Ovo"
+      >
+        Introduction
+      </motion.h4>
 
-            <motion.h4 initial={{ opacity: 0, y: -20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.2 }}
-                className='text-center mb-2 text-lg font-Ovo'>Introduction</motion.h4>
+      <motion.h2
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.3 }}
+        className="text-center text-5xl font-Ovo"
+      >
+        About Me
+      </motion.h2>
 
-            <motion.h2 initial={{ opacity: 0, y: -20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.3 }}
-                className='text-center text-5xl font-Ovo'>About Me</motion.h2>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.6 }}
+        className="flex w-full flex-col lg:flex-row gap-10 my-10"
+      >
+        <motion.div
+          initial={{ opacity: 0, scale: 0.9 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.4 }}
+          className="w-64 sm:w-80 rounded-3xl max-w-none"
+        >
+          <Image
+            src={assets.user_image}
+            alt="User Image"
+            className="w-full rounded-3xl"
+          />
+        </motion.div>
 
-            <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 0.6 }}
-                className='flex w-full flex-col lg:flex-row gap-10 my-10'>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 0.6, delay: 0.5 }}
+          className="flex-1"
+        >
+          <p className="mb-10 max-w-2xl font-Ovo">
+            I am a Frontend Web Developer with 4 years of experience in building
+            high-performance, scalable web applications. My expertise lies in
+            React and Next.js, where I create dynamic and responsive user
+            interfaces that enhance user experience. I am passionate about
+            leveraging modern web technologies to deliver innovative solutions
+            that meet client needs.
+          </p>
 
-                <motion.div initial={{ opacity: 0, scale: 0.9 }} whileInView={{ opacity: 1, scale: 1 }} transition={{ duration: 0.4 }}
-                    className='w-64 sm:w-80 rounded-3xl max-w-none'>
-                    <Image src={assets.user_image} alt="User Image" className='w-full rounded-3xl' />
-                </motion.div>
-
-                <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 0.6, delay: 0.5 }}
-                    className='flex-1'>
-
-                    <p className='mb-10 max-w-2xl font-Ovo'>
-                        I am a Frontend Web Developer with 4 years of experience in building high-performance, scalable web applications. My expertise lies in React and Next.js, where I create dynamic and responsive user interfaces that enhance user experience. I am passionate about leveraging modern web technologies to deliver innovative solutions that meet client needs.
-                    </p>
-
-                    {/* <motion.ul initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 0.8, delay: 0.6 }}
+          {/* <motion.ul initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 0.8, delay: 0.6 }}
                         className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl'>
                         {infoList.map(({ icon, iconDark, title, description }, index) => (
                             <motion.li whileHover={{ scale: 1.05 }}
@@ -41,67 +76,87 @@ const About = ({ isDarkMode }) => {
                         ))}
                     </motion.ul> */}
 
-                    <motion.ul
-                        initial={{ opacity: 0 }}
-                        whileInView={{ opacity: 1 }}
-                        transition={{ duration: 0.8, delay: 0.6 }}
-                        className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl'
-                    >
-                        {infoList.map(({ icon, iconDark, title, description }, index) => (
-                            <motion.li
-                                whileHover={{ scale: 1.05 }}
-                                key={index}
-                                onClick={() => {
-                                    if (title === "Projects") {
-                                        document.getElementById("work")?.scrollIntoView({ behavior: "smooth" });
-                                    }
-                                }}
-                                className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-black dark:border-white dark:hover:shadow-white dark:hover:bg-darkHover/50'
-                            >
-                                <Image src={isDarkMode ? iconDark : icon} alt="title" className='w-7 mt-3' />
-                                <h3 className='my-4 font-semibold text-gray-700 dark:text-white'>{title}</h3>
-                                <p className='text-gray-600 text-sm dark:text-white/80'>{description}</p>
-                            </motion.li>
-                        ))}
-                    </motion.ul>
+          <motion.ul
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 0.8, delay: 0.6 }}
+            className="grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl"
+          >
+            {infoList.map(({ icon, iconDark, title, description }, index) => (
+              <motion.li
+                whileHover={{ scale: 1.05 }}
+                key={index}
+                onClick={() => {
+                  if (title === "Projects") {
+                    document
+                      .getElementById("work")
+                      ?.scrollIntoView({ behavior: "smooth" });
+                  }
+                }}
+                className="border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-black dark:border-white dark:hover:shadow-white dark:hover:bg-darkHover/50"
+              >
+                <Image
+                  src={isDarkMode ? iconDark : icon}
+                  alt="title"
+                  className="w-7 mt-3"
+                />
+                <h3 className="my-4 font-semibold text-gray-700 dark:text-white">
+                  {title}
+                </h3>
+                <p className="text-gray-600 text-sm dark:text-white/80">
+                  {description}
+                </p>
+              </motion.li>
+            ))}
+          </motion.ul>
 
-                    <motion.h4 initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 1.3, delay: 0.5 }}
-                        className='my-6 text-gray-700 font-Ovo dark:text-white/80'>Design & Development Tools</motion.h4>
+          <motion.h4
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1.3, delay: 0.5 }}
+            className="my-6 text-gray-700 font-Ovo dark:text-white/80"
+          >
+            Design & Development Tools
+          </motion.h4>
 
-                    <motion.ul initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 1.5, delay: 0.6 }}
-                        className='flex items-center gap-3 sm:gap-5'>
-                        {/* {toolsData.map((tool, index) => (
+          <motion.ul
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 1.5, delay: 0.6 }}
+            className="flex items-center gap-3 sm:gap-5"
+          >
+            {/* {toolsData.map((tool, index) => (
                             <motion.li whileHover={{ scale: 1.1 }}
                                 className="flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500" key={index}>
                                 <Image src={tool} alt='Tool Icon' className='w-5 sm:w-7' />
                             </motion.li>
                         ))} */}
-                        {toolsData.map((tool, index) => (
-                            <motion.li
-                                whileHover={{ scale: 1.1 }}
-                                key={index}
-                                className="relative group flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500"
-                            >
-                                <Image src={tool.icon} alt={tool.name} className='w-5 sm:w-7' />
+            {toolsData.map((tool, index) => (
+              <motion.li
+                whileHover={{ scale: 1.1 }}
+                key={index}
+                className="relative group flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 rounded-lg cursor-pointer hover:-translate-y-1 duration-500"
+              >
+                <Image src={tool.icon} alt={tool.name} className="w-5 sm:w-7" />
 
-                                {/* Tooltip */}
-                                <div className="absolute -bottom-8 left-1/2 -translate-x-1/2 
+                {/* Tooltip */}
+                <div
+                  className="absolute -bottom-8 left-1/2 -translate-x-1/2 
   border border-gray-700 text-gray-800 
   dark:border-white dark:text-white
   text-xs px-2 py-1 rounded 
   opacity-0 group-hover:opacity-100 
-  transition duration-300 whitespace-nowrap backdrop-blur-sm">
-                                    {tool.name}
-                                </div>
-                            </motion.li>
-                        ))}
-                    </motion.ul>
-
-                </motion.div>
-
-            </motion.div>
+  transition duration-300 whitespace-nowrap backdrop-blur-sm"
+                >
+                  {tool.name}
+                </div>
+              </motion.li>
+            ))}
+          </motion.ul>
         </motion.div>
-    )
-}
+      </motion.div>
+    </motion.div>
+  );
+};
 
-export default About
+export default About;
